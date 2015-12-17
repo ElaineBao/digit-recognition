af751ba48a7aaf52d9ea62a20c84011c908c0125
ElaineBao <elaine.bao@foxmail.com>
2015-12-17 10:49:52 +0800

    revised

--- performRecognition.py
+++ performRecognition.py
@@ -8,33 +8,33 @@ import numpy as np
 import argparse as ap
 
 # Get the path of the training set
+'''
 parser = ap.ArgumentParser()
 parser.add_argument("-c", "--classiferPath", help="Path to Classifier File", required="True")
 parser.add_argument("-i", "--image", help="Path to Image", required="True")
 args = vars(parser.parse_args())
-
+'''
 # Load the classifier
-clf, pp = joblib.load(args["classiferPath"])
-
+#clf, pp = joblib.load(args["classiferPath"])
+clf,pp= joblib.load("digits_cls.pkl")
 # Read the input image 
-im = cv2.imread(args["image"])
-
+#im = cv2.imread(args["image"])
+im=cv2.imread("photo_2.jpg")
 # Convert to grayscale and apply Gaussian filtering
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 
 # Threshold the image
 ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
-
+cv2.imshow("threshold",im_th.copy())
 # Find contours in the image
-ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1:3]
 # Get rectangles contains each contour
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
-
 # For each rectangular region, calculate HOG features and predict
 # the digit using Linear SVM.
 for rect in rects:
+	#print rect
     # Draw the rectangles
     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
     # Make the rectangular region around the digit
@@ -42,15 +42,20 @@ for rect in rects:
     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
     pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
     roi = im_th[pt1:pt1+leng, pt2:pt2+leng]
+    if roi.size==0:
+    	continue
     # Resize the image
+	#print "roi:",roi,type(roi),roi.size
     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
     roi = cv2.dilate(roi, (3, 3))
+    cv2.imshow("roi",roi)
+    #cv2.waitKey()
     # Calculate the HOG features
     roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-    roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
-    nbr = clf.predict(roi_hog_fd)
-    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
-
-cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
+	#roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
+    nbr=clf.predict(np.array([roi_hog_fd],"float64"))
+    cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 0),3)
+    print "number:",nbr[0]
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey()
+#cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
